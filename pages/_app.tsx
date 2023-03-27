f21ffc3b99d357d308ed406e34f4f39e630b6881
yaoyao <i@yaoyao.io>
2023-03-27 11:50:38 +0800

    update css && config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,19 +5,20 @@ import { useRouter } from 'next/router'
 
 import * as Fathom from 'fathom-client'
 // used for rendering equations (optional)
+
+import 'react-notion-x/src/styles.css'
 import 'katex/dist/katex.min.css'
 import posthog from 'posthog-js'
 // used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
-import 'react-notion-x/src/styles.css'
-import 'styles/global.css'
+import 'styles/custom/index.scss';
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
-import 'styles/notion.css'
 // global style overrides for prism theme (optional)
-import 'styles/prism-theme.css'
+
+
 
 import { bootstrap } from '@/lib/bootstrap-client'
 import {
--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'bf298cd999ca4bdebf36261fe582821b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'YaoYao',
+  domain: 'notion.yaoyao.io',
+  author: 'YaoYao',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'yaoyaoio',
+  github: 'yaoyaoio',
+  linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -55,11 +55,7 @@ export default siteConfig({
   // navigationLinks: [
   //   {
   //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //     pageId: '0de39fd8ac084696933ab9a5e238a079'
   //   }
   // ]
 })
