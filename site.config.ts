bef05288222772c04b7ef8f2289a698503bf63b1
Yao Yao <i@yaoyao.io>
2023-03-27 19:06:53 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -22,7 +22,7 @@ export default siteConfig({
   linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'yaoyaoio', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
